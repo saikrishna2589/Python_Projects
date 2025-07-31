941d68572eb20dbeb0cd26791918cef4e952b8bf
ssamudrala <saikrishna2589@gmail.com>
2025-07-31 16:55:49 +1000

    coding_projects

---
+++ detect_duplicate_files_and_delete_in_folder.py
@@ -0,0 +1,57 @@
+import os
+directory ='files/detect_duplicate_files'
+
+#walk through the directory, get the root, sub-dir and files
+#get the files
+#create filepath
+#read the contents
+#pass contents to dict
+#if key already in dict, flag duplicate and then delete the duplicate file
+#print message of deletion
+#then print the files now finally in the folder after de-duplication
+
+#function to read file contents in binary format
+def file_read(file_path):
+    with open(file_path, 'rb') as filecontent:
+        file_content = filecontent.read()
+        return file_content
+
+
+filecontents_store = {}
+deleted_files =0
+files_remaining=0
+file_folder = os.walk(directory)
+
+for root, dir, files in file_folder:
+    for file in files:
+        # filepath build
+        filepath = os.path.join(root,file)
+        #read files in binary mode to get underlying contents access
+        file_details = file_read(filepath) #finished reading content.
+
+        if file_details in filecontents_store:
+            #delete file
+            os.remove(filepath)
+            deleted_files +=1
+            print(f"Duplicate file : {filepath} has been deleted")
+            print(f"Original file  : {filecontents_store[file_details]}")
+
+        else:
+            filecontents_store[file_details] = filepath
+
+
+print(f"The remaining deduplicated files:\n ")
+
+for root,dir, files in os.walk(directory):
+    for file in files:
+        files_remaining += 1
+        print(os.path.join(root,file))
+
+print(f"files remaining in the folder:{files_remaining}")
+print(f"deleted files: {deleted_files}")
+
+
+
+
+
+
